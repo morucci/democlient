b4b4046d022d7b0c19ba7f70ad3f8e1bec3b8e26
Fabien Boucher <fabien.boucher@enovance.com>
2015-06-15 16:01:05 +0200

    up1

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+SLEEP_DELAY=0
+
 echo "Flake8"
 FT=$(find . -iname "*.py" | xargs flake8)
 echo
 echo "Unittests"
 NT=$(nosetests)
 echo
+echo "Going to sleep for $SLEEP_DELAY second(s)"
+sleep $SLEEP_DELAY
 exit $[FT+NT]
